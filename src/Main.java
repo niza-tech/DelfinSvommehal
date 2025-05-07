574d9409672b2afb6edac4c468401bb2d720588f
jihadassi <jias0001@stud.kea.dk>
2025-05-07 13:25:20 +0200

    1

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import Controllers.menuController;
-import File.FileHandling;
-
-public class Main {
-    public static void main(String[] args) {
-        FileHandling.createFile("medlemsListe.txt");
-        menuController.mainMenu();
-    }
-}
