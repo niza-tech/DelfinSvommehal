df9507e200c2962b1bdb45ba53591786f6eb5a86
JVRKEA <juri0002@stud.kea.dk>
2025-05-07 11:40:27 +0200

    first

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,3 +1,19 @@
-public class FileHandling {
+import java.io.File;
+import java.io.IOException;
 
+public class FileHandling {
+    public static void createFile(String path) {
+        try {
+            File fil = new File(path);
+            if (fil.createNewFile()) {
+                System.out.println("File created: " + fil.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
 }
