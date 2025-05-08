13547757e7a42669b2de7c36329a673aba5c4d51
JVRKEA <juri0002@stud.kea.dk>
2025-05-08 09:30:29 +0200

    first

--- src/Controllers/medlemController.java
+++ src/Controllers/medlemController.java
@@ -2,8 +2,28 @@ package Controllers;
 
 import Medlem.Member;
 
+import java.util.Scanner;
+
 public class medlemController {
     public static Member opretMedlem() {
+        Scanner scanner = new Scanner(System.in);
+
+System.out.println("Navn på nyt medlem: ");
+String navn = scanner.nextLine();
+
+System.out.println("Alder på nyt medlem: ");
+int age = scanner.nextInt();
+
+System.out.println("Er den nye medlem en konkurrenceSvømmer? (true/false): ");
+boolean konkurrenceSvømmer = scanner.nextBoolean();
+scanner.nextLine();
+
+int newMemberId =
+
+Member newMember = new Member(newMemberId, navn, age, konkurrenceSvømmer);
+
+
+return newMember;
     }
 
     public static void fjernMedlem() {
