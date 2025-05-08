c64e132b957a57a677e1a4a55b8c5c2ab96b0135
niza-tech <niz@live.dk>
2025-05-08 09:55:29 +0200

    opretmedlem

--- src/Controllers/medlemController.java
+++ src/Controllers/medlemController.java
@@ -2,9 +2,39 @@ package Controllers;
 
 import Medlem.Member;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class medlemController {
+    private static ArrayList<Member> members = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static Member opretMedlem() {
-    }
+        scanner.nextLine();
+        System.out.print("Navn: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Alder: ");
+        int age = scanner.nextInt();
+        ;
+
+        System.out.print("ID: ");
+        int memberId = scanner.nextInt();
+
+        System.out.print("Aktivt medlem? (ja/nej): ");
+        boolean active = scanner.nextBoolean();
+
+        System.out.print("Restance? (ja/nej): ");
+        boolean restance = scanner.nextBoolean();
+
+        System.out.print("Konkurrencesvømmer? (ja/nej): ");
+        boolean konkurrenceSvømmer = scanner.nextBoolean();
+
+        Member member = new Member(name, age, memberId, active, restance,
+                konkurrenceSvømmer);
+        members.add(member);
+        System.out.println("Medlem oprettet: " + member.getName());
 
     public static void fjernMedlem() {
     }
